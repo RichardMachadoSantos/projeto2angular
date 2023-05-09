cf81b3df7655f2757862b40ed19d7908aa90e507
RichardMachadoSantos <richardmsantos2004@gmail.com>
2023-05-09 15:01:52 -0300

    commit geral

---
+++ crud-angular-firebase/src/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+
+  production: false,
+
+  firebaseConfig: {
+
+    apiKey: "AIzaSyADcH90j7uBTLAvTy2n17FpkEZMqHdAsSI",
+    authDomain: "projeto2angular.firebaseapp.com",
+    databaseURL: "https://projeto2angular-default-rtdb.firebaseio.com",
+    projectId: "projeto2angular",
+    storageBucket: "projeto2angular.appspot.com",
+    messagingSenderId: "484969146",
+    appId: "1:484969146:web:b07d5b070a83635e73d083",
+    measurementId: "G-PJ7RQVF3H3"
+  }
+
+};
