ec3f241a9311b8d1864557806ee462c408bea6fc
RichardMachadoSantos <richardmsantos2004@gmail.com>
2023-06-06 14:45:03 -0300

    projeto2angular

--- crud-angular-firebase/src/app/app.module.ts
+++ crud-angular-firebase/src/app/app.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 
-
-
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
-
-
 import { environment } from '../environment';
 
-
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AngularFireModule } from '@angular/fire/compat';
 
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-
-
 
 import { AddContatoComponent } from './contato/add-contato/add-contato.component';
 
@@ -30,46 +24,27 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import{ToastrModule} from 'ngx-toastr';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
 @NgModule({
-
   declarations: [
-
     AppComponent,
-
     AddContatoComponent,
-
     EditContatoComponent,
-
     ListContatoComponent
-
   ],
 
   imports: [
-
     BrowserModule,
-
     AngularFireModule.initializeApp(environment.firebaseConfig),
-
     AngularFireDatabaseModule,
-
     AppRoutingModule,
-
-    BrowserAnimationsModule, // required animations module
-
-    ToastrModule.forRoot(), // ToastrModule added
-
     FormsModule,
-
     ReactiveFormsModule,
-
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgxPaginationModule
   ],
-
   providers: [],
-
   bootstrap: [AppComponent]
 
 })
-
 export class AppModule { }
